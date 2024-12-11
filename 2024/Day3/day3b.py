109569292c305ca998655221974a2e78184ce05f
Kevin Cammarata <kcammarata@gmail.com>
2024-12-11 07:50:41 -0600

    Scrubbed files of new lines and all text between don't and do( markers

--- 2024/Day3/day3b.py
+++ 2024/Day3/day3b.py
@@ -2,14 +2,18 @@
 import re
 
 
-# file_obj = open("/Users/kevincammarata/Downloads/AdventOfCode/2024/Day3/day3test.dat", "r") 
+# file_obj = open("/Users/kevincammarata/Downloads/AdventOfCode/2024/Day3/day3btest.dat", "r") 
 file_obj = open("/Users/kevincammarata/Downloads/AdventOfCode/2024/Day3/day3data.dat", "r") 
 
 
 # reading the data from the file 
 file_data = file_obj.read() 
 # print(file_data)
-good_mults = re.findall(r'(do\(\){0,1}).*()mul\((\d{1,3}),(\d{1,3})\).*don\'t{\(\)1}',file_data)
+scrubbed_file = re.sub(r'\n', '', file_data)
+scrubbed_file = re.sub(r'don\'t.*?do\(',"",scrubbed_file)
+print(scrubbed_file)
+
+good_mults = re.findall(r'mul\((\d{1,3}),(\d{1,3})\)',scrubbed_file)
 
 sum = 0
 for pair in good_mults:
